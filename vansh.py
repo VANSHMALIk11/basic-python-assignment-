0e34787bd95f2e8b4890ac8cbcb5ae66c1164503
VANSHMALIk11 <vansh7262b@gmail.com>
2025-07-08 22:23:46 +0530

    Add files via upload

---
+++ vansh.py
@@ -0,0 +1,13 @@
+#assignment-2
+#task-1
+no =int(input("enter your first name:"))
+
+if no%2 ==0:
+    print("whose no you give",no,"is even")
+else:
+    print("whose no.you give",no,"is odd")
+#task-2
+num=0
+for i in range(1,51):
+    num=num+i
+    print("the sum of integers 1 to 50 is: ",num)
