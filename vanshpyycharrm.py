68382eeeeb6ded5bb7e52bd78200a437595fcb2d
VANSHMALIk11 <vansh7262b@gmail.com>
2025-07-08 21:40:23 +0530

    Add files via upload

---
+++ vanshpyycharrm.py
@@ -0,0 +1,18 @@
+#task-1
+m =int(input('Enter a number first no.: '))
+n=int(input('Enter a number second no.: '))
+add=m+n
+mul=m*n
+sub=m-n
+div=m//n
+rem=m%n
+
+print("addition =",add)
+print("multiplication =",mul)
+print("subtraction =",sub)
+print("division =",div)
+print("remainder =",rem)
+#task-2
+ff=input("enter  your first name:")
+fff=input("enter  your last name:")
+print("hlleo,",ff + fff,"welcome to the python program")
